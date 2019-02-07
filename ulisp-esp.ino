9da372c09e7f7cf4244b463452574e34a952f501
David Johnson-Davies <david@technoblogy.com>
2019-02-07 12:26:07 +0000

    Version 2.5c - 7th February 2019
    Fixes bug in printing floating-point numbers.

--- ulisp-esp.ino
+++ ulisp-esp.ino
@@ -1,5 +1,5 @@
-/* uLisp ESP Version 2.5 - www.ulisp.com
-   David Johnson-Davies - www.technoblogy.com - 30th November 2018
+/* uLisp ESP Version 2.5c - www.ulisp.com
+   David Johnson-Davies - www.technoblogy.com - 7th February 2019
 
    Licensed under the MIT license: https://opensource.org/licenses/MIT
 */
@@ -3680,7 +3680,13 @@ void pmantissa (float f, pfun_t pfun) {
     int d = (int)(i / mul);
     pfun(d + '0');
     i = i - d * mul;
-    if (i == 0) { if (!point) { pfun('.'); pfun('0'); } return; }
+    if (i == 0) { 
+      if (!point) { 
+        for (int k=j; k<sig; k++) pfun('0');
+        pfun('.'); pfun('0');
+      }
+      return;
+    }
     if (j == sig && sig >= 0) { pfun('.'); point = true; }
     mul = mul / 10;
   }
